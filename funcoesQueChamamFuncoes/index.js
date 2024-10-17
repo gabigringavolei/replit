20c24b17b08a6e4f66fb7600fb95d62dddb17e03
gabigringavolei <130366455+gabigringavolei@users.noreply.github.com>
2024-10-17 08:42:06 -0300

    funções que chamam funções

---
+++ funcoesQueChamamFuncoes/index.js
@@ -0,0 +1,15 @@
+/**crie um objeto de pessoas
+ * com s seguintes atributos: nome/sobrenome
+ * crie uma função que itere sobre o array
+ * e retire nomes e sobrenomes
+ * crie uma variavel com o conteudo:
+ * -> const emailBase = "@escola.pr.gov.br"
+ * a partir desses dados forme um @escola  com
+ * nome.sobre+emailbase
+ * execute a função....
+ */
+let gabi = [
+  { nome: "mari", sobrenome: "janu" },
+  { nome: "luis", sobrenome: "simeone" },
+  {},
+];
