1dd923726ef8498471d4d8b641dd85ac6ea3efb6
gabigringavolei <130366455+gabigringavolei@users.noreply.github.com>
2024-10-17 08:49:17 -0300

    operadorternario

---
+++ operadorTernario/index.js
@@ -0,0 +1,11 @@
+const idade = 18;
+
+if (idade >= 18) {
+  console.log("PODE ENTRAR");
+} else {
+  console.log("ACESSO NEGADO");
+}
+
+const passe = idade >= 18 ? "PODE ENTRAR" : "ACESSO NEGADO";
+
+console.log(passe);
