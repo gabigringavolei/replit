3637edf70e660f85561f52c5f7f7ad00cf39287a
gabigringavolei <130366455+gabigringavolei@users.noreply.github.com>
2024-10-24 07:38:16 -0300

    modulos

---
+++ moduloCJSESM/index.js
@@ -0,0 +1,6 @@
+import {os, path, fs} from "./lib/meuCarro.cjs";
+import { printNome } from "./lib/string";
+console.log(os);
+console.log(path);
+console.log (fs);
+console.log(printNome("ledson"));
---
+++ moduloCJSESM/lib/meuCarro.cjs
@@ -0,0 +1,4 @@
+const os = require('os');
+const path = require('path');
+const fs = require('fs');
+module.exports = {os, path, fs}
